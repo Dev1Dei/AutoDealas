a1cf5ad15d84782341e42f19691986a77e5dc22b
Dev1Dei <deividasbruzas333@gmail.com>
2023-02-12 03:26:53 +0200

    test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name"viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AutoDealas.ltt</title>
+    <title>AutoDealas.lt</title>
 </head>
 <body>
     <h1>AutoDealas.lt</h1>
